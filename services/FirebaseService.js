5fdc254a316bbaecc7439839299fcc1e831536ab
Parker Hewitt <Hewittparker@gmail.com>
2018-02-22 11:45:44 -0700

    add production check for require

--- services/FirebaseService.js
+++ services/FirebaseService.js
@@ -1,13 +1,18 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../secretConfigs/firebase.json");
+
+if (!(process.env.NODE_ENV === "production"))
+  var serviceAccount = require("../secretConfigs/firebase.json");
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: 'ibn-demo',
-    clientEmail: 'firebase-adminsdk-85oni@ibn-demo.iam.gserviceaccount.com',
-    privateKey: process.env.NODE_ENV === "production"? process.env.FIRESTORE_PRIVATE_KEY : serviceAccount.private_key,
+    projectId: "ibn-demo",
+    clientEmail: "firebase-adminsdk-85oni@ibn-demo.iam.gserviceaccount.com",
+    privateKey:
+      process.env.NODE_ENV === "production"
+        ? process.env.FIRESTORE_PRIVATE_KEY
+        : serviceAccount.private_key
   }),
-  databaseURL: 'https://ibn-demo.firebaseio.com'
+  databaseURL: "https://ibn-demo.firebaseio.com"
 });
 
 let db = admin.firestore();
@@ -53,6 +58,6 @@ module.exports.getCoinHistory = async coinType => {
       console.log("Error getting documents: ", error);
     });
 
-    await colRef;
-    return data;
+  await colRef;
+  return data;
 };
