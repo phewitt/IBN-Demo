151707a7e79d6e7d3889f2673f910bc70af4f167
Parker Hewitt <Hewittparker@gmail.com>
2018-02-21 19:10:51 -0700

    remove console logs

--- services/FirebaseService.js
+++ services/FirebaseService.js
@@ -42,11 +42,8 @@ module.exports.getCoinHistory = async coinType => {
     .get()
     .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
         data.push(doc.data());
-      //  console.log(doc.id, " => ", doc.data());
       });
-      console.log(data);
     })
     .catch(function(error) {
       console.log("Error getting documents: ", error);
