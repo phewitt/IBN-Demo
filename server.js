8a8afe42008d3a52632f5984d36f4506e2cd1adb
Parker Hewitt <Hewittparker@gmail.com>
2018-02-21 19:04:48 -0700

    add production check back for heroku

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 
 require("./routes/ExchangePricingRoutes")(app);
 
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
